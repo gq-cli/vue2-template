ad470d2ec64d094dd4a13ce2afd6b34ccdbb5dbe
Nuro <1617285096@qq.com>
2023-01-25 19:51:32 +0800

    自定义模板

---
+++ src/ask.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'the resgistery is private?'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'author name: '
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description for this project'
+  }
+]
